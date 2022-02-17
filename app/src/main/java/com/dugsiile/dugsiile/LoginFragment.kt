f0710c0b1183d7eb1f4536a8de6e3767d9f0f2b9
nuradin <nuradin170@gmail.com>
2022-02-17 12:35:47 +0300

    Redisign login fragment

---
+++ app/src/main/java/com/dugsiile/dugsiile/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.dugsiile.dugsiile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
+    }
+
+}
